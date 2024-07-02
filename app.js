3544790896379201d6baa498d1601d9b6a9bdb6b
Sadeepa Dilhara <sadeepadilharap@gmail.com>
2024-07-02 16:43:41 +0530

    css added

--- app.js
+++ app.js
@@ -3,30 +3,31 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-var items = ["Buy food", "cook food", "eat food"];
+let items = ["Buy food", "cook food", "eat food"];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended : true}));
+app.use(express.static('public'));
 
 app.get("/", function (req, res) {
 
-    var today = new Date();
+    let today = new Date();
 
-    var options = {
+    let options = {
         weekday: "long",
         day: "numeric",
         month: "long"
     };
 
-    var day = today.toLocaleDateString("en-US", options);
+    let day = today.toLocaleDateString("en-US", options);
 
     res.render("list", { kindOfDay: day , newListItems: items})
 
 });
 
 app.post("/", function(req, res){
-    var item = req.body.newItem;
+    let item = req.body.newItem;
     console.log(item);
     items.push(item);
 
