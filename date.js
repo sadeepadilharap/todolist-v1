c1db0725ec8f6c10921c7d34e721fa802ab740b2
Sadeepa Dilhara <sadeepadilharap@gmail.com>
2024-07-02 18:39:17 +0530

    getDay and getDate functions added

--- date.js
+++ date.js
@@ -1,4 +1,4 @@
-module.exports = getDate;
+module.exports.getDate = getDate;
 
 function getDate() {
 
@@ -8,6 +8,21 @@ function getDate() {
         weekday: "long",
         day: "numeric",
         month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    return day;
+}
+
+module.exports.getDay = getDay;
+
+function getDay() {
+
+    let today = new Date();
+
+    let options = {
+        weekday: "long"
     };
 
     let day = today.toLocaleDateString("en-US", options);
