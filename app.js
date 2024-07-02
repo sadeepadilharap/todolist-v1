2f9340d47672eaede550b9053fe5312bd6041403
Sadeepa Dilhara <sadeepadilharap@gmail.com>
2024-07-02 18:36:30 +0530

    date module added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -13,15 +14,7 @@ app.use(express.static('public'));
 
 app.get("/", function (req, res) {
 
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+    let day = date();
 
     res.render("list", { listTitle: day , newListItems: items})
 
